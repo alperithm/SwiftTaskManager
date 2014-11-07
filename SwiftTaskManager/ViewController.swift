7607db593ac9611933b4baf947170a01b5a24a72
ALPEN <alperithm@gmail.com>
2014-11-08 03:13:15 +0900

    first commit

---
+++ SwiftTaskManager/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  SwiftTaskManager
+//
+//  Created by ALPEN on 2014/11/07.
+//  Copyright (c) 2014年 alperithm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // セルに表示するテキスト
+    let texts = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    
+    // セルの行数
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return texts.count
+    }
+
+    // セルの内容を更新
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Cell")
+        
+        cell.textLabel.text = texts[indexPath.row]
+        cell.textLabel.textAlignment = NSTextAlignment.Center
+        return cell
+    }
+    
+    
+}
+
