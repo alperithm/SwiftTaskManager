dd4b25161fd97cc05f3ceabeb6c5d14fbc1cdeec
ALPEN <alperithm@gmail.com>
2014-11-08 03:51:26 +0900

    テキストフィールドの入力をセルに追加する

--- SwiftTaskManager/ViewController.swift
+++ SwiftTaskManager/ViewController.swift
@@ -8,14 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var addTextField: UITextField!
+    
+    // セルに表示するテキスト
+    var texts = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // TableViewデリゲートメソッド利用準備
         tableView.delegate = self
         tableView.dataSource = self
+        
+        // TextFieldデリゲートメソッド利用準備
+        addTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,8 +31,9 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         // Dispose of any resources that can be recreated.
     }
     
-    // セルに表示するテキスト
-    var texts = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    /**
+    *   TableViewデリゲートメソッド
+    */
     
     // セルの行数
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -42,9 +51,19 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     // セルを追加する
     @IBAction func addTableCellButton(sender: AnyObject) {
-        println("add Cell")
-        texts.append(String(format: "testCell%02d", texts.count))
+        texts.append(addTextField.text)
         tableView.reloadData()
+        addTextField.text = String()
+    }
+    
+    /**
+    *   TextFieldデリゲートメソッド
+    */
+    
+    // テキストフィールド入力終了後キーボードを閉じる
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        self.view.endEditing(true)
+        return false
     }
 }
 
